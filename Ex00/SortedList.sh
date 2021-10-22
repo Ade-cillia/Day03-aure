ee714197a579fac35c40716d9b808ebbdf50b04c
AurélienDECILLIA <a.de-cillia@it-students.fr>
2021-10-22 10:11:02 +0200

    Ex00

--- Ex00/SortedList.sh
+++ Ex00/SortedList.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-#occArr[i]=($i+system('cat histoire.txt  | tr ' ' '\n' | grep -c "the"'))
-#for (key in occArr) { print occArr[key] }
-#marche pas
-awk -F ' ' 'BEGIN {i=1} {
-    while ($i) {
-        occArr[$i]=system("cat histoire.txt | tr \" \"  \"\n\" | grep -c "$i" ")
-        i++
+#occArr[i]=($i+system('cat histoire.txt  | tr ' ' '\n' | grep -c "$i"'))
+
+awk -F ' ' '
+BEGIN {i=1;countOccu=1}{ 
+    while ($i) { 
+        k=1;
+        while ($k) {
+            if($k==$i){
+                countOccu++
+            };
+            k++;  
+        };
+        occArr[$i]=countOccu;
+        countOccu=0;
+        i++;
     }
-} END {for (key in occArr) { print key" "occArr[key] }}' histoire.txt
+} END { 
+    for (key in occArr) { print key" "occArr[key] } 
+}' histoire.txt
+
+#Version ligne:
+# awk -F ' ' 'BEGIN {i=1;countOccu=1}{ while ($i) { k=1;while ($k) { if($k==$i){ countOccu++ }; k++;  }; occArr[$i]=countOccu; countOccu=0; i++; } } END { for (key in occArr) { print key" "occArr[key] } }' histoire.txt
